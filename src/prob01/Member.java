3b18fac3c7523b55a20898d37b4066e462a0d318
jungeuidon <mantopuma@naver.com>
2019-08-09 16:50:21 +0900

    1번 완료 /StringUtil 4번참조

--- src/prob01/Member.java
+++ src/prob01/Member.java
@@ -1,4 +1,28 @@
 package prob01;
 
 public class Member {
+	String id;
+	String name;
+	int point;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	
+	
 }
---
+++ src/prob01/StringUtil.java
@@ -0,0 +1,16 @@
+package prob01;
+
+public class StringUtil {
+	
+	public static String concatenate(String[] str) {
+		
+		String result = "";
+		for (int i = 0; i < str.length; i++) {
+			result += str[i];
+		}
+			
+		
+		
+		return result;
+    }
+}
